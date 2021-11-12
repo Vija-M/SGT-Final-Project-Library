18374fc5e470257801fb206669a61d8762161e64
Vija-M <vija.riga@gmailcom>
2021-11-12 19:44:07 +0200

    UserAccess class creation

---
+++ src/objects/UserAccess.java
@@ -0,0 +1,48 @@
+package objects;
+
+import java.util.StringJoiner;
+
+public class UserAccess {
+    private int userAccessID;
+    private int userID;
+    private int roleID;
+
+    public UserAccess(int userAccessID, int userID, int roleID) {
+        this.userAccessID = userAccessID;
+        this.userID = userID;
+        this.roleID = roleID;
+    }
+
+    public int getUserAccessID() {
+        return userAccessID;
+    }
+
+    public void setUserAccessID(int userAccessID) {
+        this.userAccessID = userAccessID;
+    }
+
+    public int getUserID() {
+        return userID;
+    }
+
+    public void setUserID(int userID) {
+        this.userID = userID;
+    }
+
+    public int getRoleID() {
+        return roleID;
+    }
+
+    public void setRoleID(int roleID) {
+        this.roleID = roleID;
+    }
+
+    @Override
+    public String toString() {
+        return new StringJoiner(", ", UserAccess.class.getSimpleName() + "[", "]")
+                .add("userAccessID=" + userAccessID)
+                .add("userID=" + userID)
+                .add("roleID=" + roleID)
+                .toString();
+    }
+}
