1ffa8b014a41002911dc62ba7f9cedf5d29c6e58
Vija-M <vija.riga@gmailcom>
2021-11-14 19:50:25 +0200

    Add user function creation

--- src/menu/PrintMenu.java
+++ src/menu/PrintMenu.java
@@ -5,44 +5,52 @@ import java.util.Scanner;
 import objects.Books;
 
 public class PrintMenu {
+
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in); //monitors console for an input
 
         System.out.println("********************Welcome to the Rustic Library!********************");
         System.out.println();
-        System.out.println("Please enter your clientID");
-        String clientID = scanner.next();
-
-        // 1) retrieve info from the database about this user.
-        // 2) Print it out.
-
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please enter 1 if you are a client or 2 if you are a librarian.");
+        if (sc.nextInt() == 2) {
+            System.out.println("Please enter your userID");
+            String userID = scanner.next();
+            PrintMenu.printLibrarianMenu();
+        } else {
+            System.out.println("Please enter your clientID");
+            String clientID = scanner.next();
+            // 1) retrieve info from the database about this user.
+            // 2) Print it out.
+            PrintMenu.printMenu();
+        }
     }
 
-    private static void printMenu() {
-        {
-            Scanner sc = new Scanner(System.in); //monitors console for an input
 
-            System.out.println("Welcome to the Rustic Library!");
-            System.out.println("How can we help?");
-            System.out.println();
-            System.out.println("1: See the catalog.");
-            System.out.println("2: Search the book by title.");
-            System.out.println("3: Search the book by an author.");
-            System.out.println("4: Search the book by its ISBN number.");
-            System.out.println("0: Exit");
-            System.out.println();
+    static void printMenu() {
+        Scanner scanner = new Scanner(System.in); //monitors console for an input
 
-            int inputSelection = sc.nextInt();
+        System.out.println("Welcome to the Rustic Library!");
+        System.out.println("How can we help?");
+        System.out.println();
+        System.out.println("1: See the catalog.");
+        System.out.println("2: Search the book by title.");
+        System.out.println("3: Search the book by an author.");
+        System.out.println("4: Search the book by its ISBN number.");
+        System.out.println("0: Exit");
+        System.out.println();
 
-            switch (inputSelection) {
-                case 1:
-                    System.out.println("Catalog: Rustic Library");
-                    // ... leads to the catalog, I guess ....
-                    break;
-                case 2:
-                    System.out.println("Please enter the title of the book.");
-                    String titleBook = scanner.next();
+        int inputSelection = scanner.nextInt();
+
+        switch (inputSelection) {
+            case 1:
+                System.out.println("Catalog: Rustic Library");
+                // ... leads to the catalog, I guess ....
+                break;
+            case 2:
+                System.out.println("Please enter the title of the book.");
+                String titleBook = scanner.next();
                         /* 1) Compare info with the database records.
 
                               if bookTitle = Books.title {
@@ -52,10 +60,10 @@ public class PrintMenu {
 
                            2) boolean yes/no -> yes: return to the menu; no: exit switch
                          */
-                    break;
-                case 3:
-                    System.out.println("Please enter full author's name.");
-                    String authorName = scanner.next();
+                break;
+            case 3:
+                System.out.println("Please enter full author's name.");
+                String authorName = scanner.next();
                     /* 1) Compare the information with that in the database
                           Something like this:
 
@@ -67,14 +75,14 @@ public class PrintMenu {
                        }
                      */
 
-                    // 2) then System.out.println("These are the books by authorId");
-                    // 3) Create an array by this author's books.
-                    // 4) print out the results
-                    // 5) Make a selection case with another switch.
-                    break;
-                case 4:
-                    System.out.println("Please enter the ISBN number of the book.");
-                    String isbnNumber = scanner.next();
+                // 2) then System.out.println("These are the books by authorId");
+                // 3) Create an array by this author's books.
+                // 4) print out the results
+                // 5) Make a selection case with another switch.
+                break;
+            case 4:
+                System.out.println("Please enter the ISBN number of the book.");
+                String isbnNumber = scanner.next();
                     /* 1) Compare the information with that in the database
                           Something like this:
                        int i = 0; //that's the row number
@@ -86,15 +94,48 @@ public class PrintMenu {
 
                     2) Print out book information or information about its absence.
                      */
-                    break;
-                case 0:
-                    return;
-                default:
-                    System.out.println("Did not recognize this selection, please try again!");
-                    break;
-            }
-            printMenu();
+                break;
+            case 0:
+                return;
+            default:
+                System.out.println("Did not recognize this selection, please try again!");
+                break;
         }
-
+        printMenu();
     }
+
+
+    static void printLibrarianMenu() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Welcome to the internal system of Rustic Library!");
+        System.out.println("Please, choose one of the options below.");
+        System.out.println();
+        System.out.println("1--> choose action with BOOKS: ");
+        System.out.println("2--> choose action with AUTHORS: ");
+        System.out.println("3--> choose action with restaurant USERS: ");
+        System.out.println("0--> EXIT! ");
+        System.out.println();
+
+        int inputSelection = scanner.nextInt();
+
+        switch (inputSelection) {
+            case 1:
+                //         BooksMenu.printMenu();
+                break;
+            case 2:
+                AuthorsMenu.printMenu();
+                break;
+            case 3:
+                //          UsersMenu.printMenu();
+                break;
+            case 0:
+                return;
+            default:
+                System.out.println("Did not recognize this selection, please try again!");
+                break;
+        }
+        printMenu();
+    }
+
 }
