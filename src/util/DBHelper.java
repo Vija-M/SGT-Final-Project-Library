6cef8d606c60fe75975472f277804b5302feb065
Vija-M <vija.riga@gmailcom>
2021-11-21 17:43:11 +0200

    User Contoller changes

--- src/util/DBHelper.java
+++ src/util/DBHelper.java
@@ -14,7 +14,8 @@ public class DBHelper {
         try {
             connection = DriverManager.getConnection(
                     // !!! Change to your db location !!!//
-                    "jdbc:sqlite:/Users/37126/SQLITE3/Library.db");
+                    "jdbc:sqlite:/Users/Everita/IdeaProjects/SGT-Final-Project-Library/sql/library.db");
+                   //Vija -  "jdbc:sqlite:/Users/37126/SQLITE3/Library.db");
             statement = connection.createStatement();
         } catch (
                 SQLException exception) {
